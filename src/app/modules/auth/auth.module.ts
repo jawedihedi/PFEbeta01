c30db418e4231f44541682965c74f4fa957d0d37
mariemhajjem <mariemhajjem10@gmail.com>
2019-03-13 16:55:49 +0100

    no

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { AuthRoutingModule } from './auth-routing.module';
+ 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    AuthRoutingModule
+    CommonModule 
   ]
 })
 export class AuthModule { }
--- src/app/modules/formation/formation.module.ts
+++ src/app/modules/formation/formation.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { FormationRoutingModule } from './formation-routing.module';
+ 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    FormationRoutingModule
+    CommonModule 
   ]
 })
 export class FormationModule { }
